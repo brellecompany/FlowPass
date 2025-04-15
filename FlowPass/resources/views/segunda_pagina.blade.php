0ec6b79d6f510412541e589b6f0f1345bf75da54
Aluno <isabella.ssantoss3004@gmail.com>
2025-04-15 16:26:16 -0300

    Front

--- FlowPass/resources/views/segunda_pagina.blade.php
+++ FlowPass/resources/views/segunda_pagina.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<header>
+    <header>
         <div class="container">
             <div id="branding">
                 <h1>FLOWPASS</h1>
@@ -23,56 +23,56 @@
             </nav>
         </div>
     </header>
+  
+    <h1>Fale com Especialistas</h1>
 
-<h1>Fale com Especialistas</h1>
+    <h2>Nossos Especialistas</h2>
+    <div class="especialistas">
+      <div class="especialista">
+        <h3>Sarah Campos Andrade</h3>
+        <p><strong>Email:</strong> sarah.campos21@gmail.com</p>
+        <p><strong>Telefone:</strong> (12) 99140-9126</p>
+      </div>
 
-<h2>Nossos Especialistas</h2>
-<div class="especialistas">
-  <div class="especialista">
-    <h3>Sarah Campos Andrade</h3>
-    <p><strong>Email:</strong> sarah.campos21@gmail.com</p>
-    <p><strong>Telefone:</strong> (12) 99140-9126</p>
+      <div class="especialista">
+        <h3>Catharina Silva Passos</h3>
+        <p><strong>Email:</strong> catharinapassos_0@gmail.com</p>
+        <p><strong>Telefone:</strong> (12) 99881-6522</p>
+      </div>
+
+      <div class="especialista">
+        <h3>Henrico Costa Felício</h3>
+        <p><strong>Email:</strong> henricoespresario@gmail.com</p>
+        <p><strong>Telefone:</strong> (12) 98823-1678</p>
+      </div>
+    </div>
+
+    <form action="#" method="post">
+      <h2>Envie uma mensagem</h2>
+
+      <label for="nome">Seu nome</label>
+      <input type="text" id="nome" name="nome" required>
+
+      <label for="email">Seu e-mail</label>
+      <input type="email" id="email" name="email" required>
+
+      <label for="assunto">Escolha um especialista</label>
+      <select id="assunto" name="assunto" required>
+        <option value="">Selecione</option>
+        <option value="fernanda">Sarah Campos Andrade</option>
+        <option value="lucas">Catharina Silva Passos</option>
+        <option value="juliana">Henrico Costa Felício</option>
+      </select>
+
+      <label for="mensagem">Mensagem</label>
+      <textarea id="mensagem" name="mensagem" rows="5" required></textarea>
+
+      <button class="botao2" type="submit">Enviar Mensagem</button>
+
+    </form>
   </div>
-
-  <div class="especialista">
-    <h3>Catharina Silva Passos</h3>
-    <p><strong>Email:</strong> catharinapassos_0@gmail.com</p>
-    <p><strong>Telefone:</strong> (12) 99881-6522</p>
-  </div>
-
-  <div class="especialista">
-    <h3>Henrico Costa Felício</h3>
-    <p><strong>Email:</strong> henricocf@gmail.com</p>
-    <p><strong>Telefone:</strong> (12) 98823-1678</p>
-  </div>
-</div>
-
-<form action="#" method="post">
-  <h2>Envie uma mensagem</h2>
-
-  <label for="nome">Seu nome</label>
-  <input type="text" id="nome" name="nome" required>
-
-  <label for="email">Seu e-mail</label>
-  <input type="email" id="email" name="email" required>
-
-  <label for="assunto">Escolha um especialista</label>
-  <select id="assunto" name="assunto" required>
-    <option value="">Selecione</option>
-    <option value="fernanda">Sarah Campos Andrade</option>
-    <option value="lucas">Catharina Silva Passos</option>
-    <option value="juliana">Henrico Costa Felício</option>
-  </select>
-
-  <label for="mensagem">Mensagem</label>
-  <textarea id="mensagem" name="mensagem" rows="5" required></textarea>
-
-  <button type="submit">Enviar Mensagem</button>
-
-</form>
-</div>
-<footer>
-<p>FlowPass &copy; 2025</p>
+  <footer>
+    <p>FlowPass &copy; 2025</p>
 </footer>
 </body>
 </html>
--- FlowPass/resources/views/welcome.blade.php
+++ FlowPass/resources/views/welcome.blade.php
@@ -1,14 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-=======
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -17,7 +7,6 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased">
->>>>>>> a401f27aecdc6b0362d4e0d1464ed03c04e77fe7
     <header>
         <div class="container">
             <div id="branding">
@@ -40,17 +29,10 @@
                 <div class="text-content">
                     <h1>Sua Central de Gerenciamento de Entrada e Saída Remota</h1>
                     <p><em>O FLOWPASS é o único software pensado para aumentar a performance do controle de acessos para a sua portaria.</em></p>
-<<<<<<< HEAD
-                    <button class="cta-button"><strong>FALE COM UM ESPECIALISTA</strong></button>
-                </div>
-                <div class="image-content">
-                    <img src="{{ asset('storage/img/mulher.png') }}" alt="Descrição da imagem">
-=======
                     <button class="cta-button"><a href="{{ route('segunda.pagina') }}" class="button"><strong>FALE COM UM ESPECIALISTA</strong></a></button>
                 </div>
                 <div class="image-content">
                     <img src="https://i.pinimg.com/736x/29/60/04/296004fd8736a412a6e0302e5047cfc9.jpg" alt="Descrição da imagem">
->>>>>>> a401f27aecdc6b0362d4e0d1464ed03c04e77fe7
                 </div>
             </div>
         </div>
@@ -65,19 +47,10 @@
             </form>
         </div>
     </section>
-<<<<<<< HEAD
-
-=======
->>>>>>> a401f27aecdc6b0362d4e0d1464ed03c04e77fe7
     <footer>
         <p>FlowPass &copy; 2025</p>
     </footer>
 
     <script src="script.js"></script>
 </body>
-<<<<<<< HEAD
-</html>
-</body>
-=======
->>>>>>> a401f27aecdc6b0362d4e0d1464ed03c04e77fe7
 </html>
